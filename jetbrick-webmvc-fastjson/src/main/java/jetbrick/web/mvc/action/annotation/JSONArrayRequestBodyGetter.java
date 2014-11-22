cde727657e8ecb9e4abe11083b9ed65ee63acaa0
Guoqiang Chen <subchen@gmail.com>
2014-11-22 17:50:23 +0800

    implements fastjson RequestBodyGetters

---
+++ jetbrick-webmvc-fastjson/src/main/java/jetbrick/web/mvc/action/annotation/JSONArrayRequestBodyGetter.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2013-2014 Guoqiang Chen, Shanghai, China. All rights reserved.
+ *
+ *   Author: Guoqiang Chen
+ *    Email: subchen@gmail.com
+ *   WebURL: https://github.com/subchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.web.mvc.action.annotation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.http.HttpServletRequest;
+import jetbrick.io.IoUtils;
+import jetbrick.web.mvc.RequestContext;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+public final class JSONArrayRequestBodyGetter implements RequestBodyGetter<JSONArray> {
+
+    @Override
+    public JSONArray get(RequestContext ctx) {
+        HttpServletRequest request = ctx.getRequest();
+        InputStream is = null;
+        try {
+            is = request.getInputStream();
+            String body = IoUtils.toString(is, request.getCharacterEncoding());
+            return JSON.parseArray(body);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        } finally {
+            IoUtils.closeQuietly(is);
+        }
+    }
+
+}
